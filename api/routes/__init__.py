8a703c926cb986f6e75bc72802901da7274a2bc5
Javier Lara <javier.lara@trocafone.com>
2016-11-26 16:19:17 -0300

    Login redirections

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -2,7 +2,8 @@ from flask.ext.restful import Api
 from api import app
 from .resources.health_center import HealthCenter, HealthCenterCollection
 from .resources.news import NewsCollection, News
-from flask import render_template, request, session, redirect, url_for, flash
+from flask import render_template, request, session, redirect, url_for, flash, g
+from functools import wraps
 
 api = Api(app)
 
@@ -12,45 +13,72 @@ api.add_resource(News, '/api/news/<string:news_id>')
 api.add_resource(NewsCollection, '/api/news')
 
 
+def logged():
+    return 'logged_in' in session and session['logged_in']
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not logged():
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 @app.route('/health_centers/new', methods=['GET'])
+@login_required
 def new_center():
     return render_template('newCenter.html')
 
 
 @app.route('/health_centers', methods=['GET'])
+@login_required
 def health_centers():
     centers = HealthCenterCollection.get_all_health_centers()
     return render_template('listCenters.html', centers=centers)
 
 
 @app.route('/health_centers/edit/<int:center_id>', methods=['GET'])
+@login_required
 def edit_center(center_id):
     center = HealthCenter.get_health_center(center_id)
     return render_template('newCenter.html', center=center)
 
 
 @app.route('/news/new', methods=['GET'])
+@login_required
 def new_news():
     return render_template('newNews.html')
 
 
 @app.route('/news', methods=['GET'])
+@login_required
 def newses():
     newses = NewsCollection.get_all_news()
     return render_template('listNews.html', newses=newses)
 
 
 @app.route('/news/edit/<int:news_id>', methods=['GET'])
+@login_required
 def edit_news(news_id):
     news = News.get_news(news_id)
     return render_template('newNews.html', news=news)
 
 
+@app.route('/home', methods=['GET'])
+@login_required
+def home():
+    return render_template('home.html')
+
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    next = request.args.get('next')
+    if not next:
+        next = url_for('home')
     if 'logged_in' in session and session['logged_in']:
-        return redirect(url_for('health_centers'))
+        return redirect(next)
     error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
@@ -60,8 +88,8 @@ def login():
         else:
             session['logged_in'] = True
             flash('Iniciaste sesión')
-            return redirect(url_for('health_centers'))
-    return render_template('login.html', error=error)
+            return redirect(request.form['next'])
+    return render_template('login.html', error=error, next=next)
 
 
 @app.route('/logout')
@@ -69,3 +97,5 @@ def logout():
     session.pop('logged_in', None)
     flash('Saliste de la sesión')
     return redirect(url_for('login'))
+
+
