f7d40ed05773b821170c2081449371f2d16fd5e3
Javier Lara <javier.lara@trocafone.com>
2017-02-20 20:48:31 -0300

    testing more for tz

--- api/models/utils.py
+++ api/models/utils.py
@@ -3,5 +3,12 @@ import pytz
 
 
 def get_time(milliseconds):
+    # tz = pytz.timezone('America/Argentina/Buenos_Aires')
+    # return datetime.fromtimestamp(float(milliseconds)/1000.0, tz)
+    utc_dt = datetime.utcfromtimestamp(float(milliseconds)/1000.0)
+    aware_utc_dt = utc_dt.replace(tzinfo=pytz.utc)
     tz = pytz.timezone('America/Argentina/Buenos_Aires')
-    return datetime.fromtimestamp(float(milliseconds)/1000.0, tz)
+    dt = aware_utc_dt.astimezone(tz)
+    print(dt)
+    return dt
+
